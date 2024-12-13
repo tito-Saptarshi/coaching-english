0b61d4eef5636fa329dcef980b5ca244cbbef8f3
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-12-13 13:02:20 +0530

    -m 13122024 0.2

--- app/components/PaymentsForms.tsx
+++ app/components/PaymentsForms.tsx
@@ -157,7 +157,7 @@ export function PaymentsForm({ paymentData, paymentPrice }: { paymentData: BankD
       <div className="grid gap-8 md:grid-cols-2">
         <Card>
           <CardHeader>
-            <CardTitle>Payment Details (${paymentPrice}/month)</CardTitle>
+            <CardTitle>Payment Details (Rs {paymentPrice} / month)</CardTitle>
             <CardDescription>
               Scan the QR code or use the account details below to make your
               payment
